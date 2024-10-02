5d867da81133dbc0ea7d12be125b9a7f66d85ecc
ppkekw <accplaceholder311@gmail.com>
2024-10-02 19:58:40 +0530

    exercise ui final

--- lib/features/exercise/exercise_screen.dart
+++ lib/features/exercise/exercise_screen.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:audioplayers/audioplayers.dart'; // Add this
+import 'package:audioplayers/audioplayers.dart';
 
 class ExerciseScreen extends StatefulWidget {
   final String exercise;
@@ -17,31 +18,28 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
   late Timer _timer;
   int _totalDuration = 0;
   int _elapsedTime = 0;
-  int _instructionElapsedTime = 0; // New variable for instruction timing
+  int _instructionElapsedTime = 0;
   int _currentPhaseIndex = 0;
   String _currentInstruction = 'Get ready...';
-
-  // Audio player instance
   late AudioPlayer _audioPlayer;
+  bool _isFadingOut = false;
+  double _opacity = 1.0; // Start fully visible
 
   @override
   void initState() {
     super.initState();
-
-    // Initialize AudioPlayer and start the music
     _audioPlayer = AudioPlayer();
     _playMusic();
-
-    // Get total duration and ensure it's at least 60 seconds
     _totalDuration = getTotalDurationForExercise(widget.exercise) ?? 120;
     if (_totalDuration < 120) {
-      _totalDuration = 120; // Ensure minimum duration is 60 seconds
+      _totalDuration = 120;
     }
     startTimer();
   }
 
   int? getTotalDurationForExercise(String exercise) {
-    return exerciseSteps[exercise]?.fold<int>(0, (total, step) => total + (step['duration'] as int));
+    return exerciseSteps[exercise]
+        ?.fold<int>(0, (total, step) => total + (step['duration'] as int));
   }
 
   void startTimer() {
@@ -49,60 +47,70 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
       setState(() {
         if (_elapsedTime < _totalDuration) {
           _elapsedTime++;
-          _instructionElapsedTime++; // Increment instruction elapsed time independently
-          updateCurrentInstruction();
+          _instructionElapsedTime++;
+          if (_instructionElapsedTime ==
+              (exerciseSteps[widget.exercise]![_currentPhaseIndex]['duration']
+                  as int)) {
+            // Start fading out
+            _fadeOutInstruction();
+          }
         } else {
-          // Show dialog when exercise is completed
           _showCompletionDialog();
-          // Cancel the timer
           _timer.cancel();
         }
       });
     });
   }
 
-  // Play music function
   Future<void> _playMusic() async {
     try {
-      // Generate a random number between 1 and 4
-      int randomFileNumber = Random().nextInt(4) + 1; // Generates 1, 2, 3, or 4
-
-      // Load audio from assets using the randomly chosen file
-      await _audioPlayer.setSource(AssetSource('music/$randomFileNumber.mp3')); // Play the random file
-      await _audioPlayer.resume(); // Start playing the audio
+      int randomFileNumber = Random().nextInt(4) + 1;
+      await _audioPlayer.setSource(AssetSource('music/$randomFileNumber.mp3'));
+      await _audioPlayer.resume();
     } catch (e) {
-      print('Error loading audio: $e'); // Error handling
+      print('Error loading audio: $e');
     }
   }
 
+  void _fadeOutInstruction() {
+    setState(() {
+      _opacity = 0.0;
+      _isFadingOut = true;
+    });
 
-  void updateCurrentInstruction() {
+    // Wait for the fade-out to complete before changing the instruction
+    Future.delayed(Duration(seconds: 1), () {
+      _updateInstruction();
+      _fadeInInstruction();
+    });
+  }
+
+  void _fadeInInstruction() {
+    setState(() {
+      _opacity = 1.0;
+      _isFadingOut = false;
+    });
+  }
+
+  void _updateInstruction() {
     if (exerciseSteps[widget.exercise] != null) {
       int totalSteps = exerciseSteps[widget.exercise]!.length;
-      int timePassed = 0;
 
-      // Update current instruction based on instruction elapsed time
-      for (int i = 0; i < totalSteps; i++) {
-        int stepDuration = exerciseSteps[widget.exercise]![i]['duration'] as int;
-        if (_instructionElapsedTime >= timePassed && _instructionElapsedTime < timePassed + stepDuration) {
-          _currentInstruction = exerciseSteps[widget.exercise]![i]['instruction'];
-          _currentPhaseIndex = i;
-          break;
-        }
-        timePassed += stepDuration;
-
-        // Cycle back to the beginning if we reach the end
-        if (i == totalSteps - 1 && _instructionElapsedTime >= timePassed) {
-          _instructionElapsedTime -= totalSteps * stepDuration; // Reset instruction elapsed time for cycling
-          _currentPhaseIndex = 0; // Reset current phase index
-        }
+      if (_currentPhaseIndex < totalSteps - 1) {
+        _currentPhaseIndex++;
+      } else {
+        _currentPhaseIndex = 0;
       }
+
+      _currentInstruction =
+          exerciseSteps[widget.exercise]![_currentPhaseIndex]['instruction'];
+      _instructionElapsedTime = 0; // Reset elapsed time for the new instruction
     }
   }
 
   void _showCompletionDialog() {
-     _audioPlayer.stop(); // Stop the music when the widget is disposed
-    _audioPlayer.dispose(); // Dispose the player to free resources
+    _audioPlayer.stop();
+    _audioPlayer.dispose();
     showDialog(
       context: context,
       builder: (context) {
@@ -113,8 +121,8 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
             TextButton(
               child: Text('OK'),
               onPressed: () {
-                Navigator.of(context).pop(); // Close dialog
-                Navigator.of(context).pop(); // Go back to previous screen
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
               },
             ),
           ],
@@ -127,8 +135,8 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
   void dispose() {
     _timer.cancel();
     super.dispose();
-    _audioPlayer.stop(); // Stop the music when the widget is disposed
-    _audioPlayer.dispose(); // Dispose the player to free resources
+    _audioPlayer.stop();
+    _audioPlayer.dispose();
   }
 
   @override
@@ -138,38 +146,48 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.exercise),
-        backgroundColor: Colors.green.shade400, // Match the style
+        leading: CupertinoButton(
+            child: Icon(
+              Icons.arrow_back_ios_new_rounded,
+              color: Colors.grey.shade800,
+              size: 18,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        title: Text(
+          widget.exercise,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+        centerTitle: true,
       ),
       body: Container(
         width: screenWidth,
         height: screenHeight,
-        color: Colors.white, // Background color for the page
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Instruction container
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.green.shade400,
-                borderRadius: BorderRadius.circular(12),
-              ),
+            SizedBox(height: 20),
+            // Instruction container with fading animation
+            AnimatedOpacity(
+              opacity: _opacity,
+              duration: Duration(seconds: 1), // Fade duration
               child: Text(
                 _currentInstruction,
-                style: TextStyle(fontSize: 24, color: Colors.white),
+                style: TextStyle(
+                    fontSize: 30, color: Color.fromRGBO(106, 172, 67, 1)),
                 textAlign: TextAlign.center,
               ),
             ),
-            SizedBox(height: 10),
+            Spacer(),
             // Circular background for the animation
             Container(
               width: screenWidth * 0.6,
               height: screenWidth * 0.6,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                color: Colors.grey.shade100, // Darker green for circular background
+                color: Colors.grey.shade100,
               ),
               child: Center(
                 child: ClipOval(
@@ -177,19 +195,28 @@ class _ExerciseScreenState extends State<ExerciseScreen> {
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            Spacer(),
             // Timer container
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.green.shade400,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Text(
-                'Time remaining: ${_totalDuration - _elapsedTime} seconds',
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Time remaining: ',
+                  style: TextStyle(
+                    color: Color.fromRGBO(106, 172, 67, 1),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  '${_totalDuration - _elapsedTime} seconds',
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ),
+              ],
             ),
+            SizedBox(
+              height: 20,
+            )
           ],
         ),
       ),
@@ -215,12 +242,14 @@ final Map<String, List<Map<String, dynamic>>> exerciseSteps = {
     {'instruction': 'Exhale for 8 seconds', 'duration': 8},
   ],
   'Alternate Nostril Breathing': [
-    {'instruction': 'Close right nostril and inhale through left nostril', 'duration': 6},
+    {
+      'instruction': 'Close right nostril and inhale through left nostril',
+      'duration': 6
+    },
     {'instruction': 'Hold breath, close both nostrils', 'duration': 6},
     {'instruction': 'Open right nostril and exhale', 'duration': 6},
     {'instruction': 'Repeat on the other side', 'duration': 6},
   ],
-  // New exercises
   'Happy Breathing': [
     {'instruction': 'Inhale slowly while smiling', 'duration': 6},
     {'instruction': 'Hold the breath, focus on happy thoughts', 'duration': 8},
@@ -234,11 +263,14 @@ final Map<String, List<Map<String, dynamic>>> exerciseSteps = {
   'Stress Relief Breathing': [
     {'instruction': 'Inhale deeply, hold for a moment', 'duration': 6},
     {'instruction': 'Exhale slowly, releasing tension', 'duration': 6},
-    {'instruction': 'Focus on releasing stress with each breath', 'duration': 6},
+    {
+      'instruction': 'Focus on releasing stress with each breath',
+      'duration': 6
+    },
   ],
   'Relaxed Mind Breathing': [
     {'instruction': 'Inhale deeply through your nose', 'duration': 6},
     {'instruction': 'Hold and relax your mind', 'duration': 8},
-    {'instruction': 'Exhale completely', 'duration': 8},
+    {'instruction': 'Exhale slowly', 'duration': 6},
   ],
 };
